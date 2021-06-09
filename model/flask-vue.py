10fadb4900d7af13b8f56752d527b41c462656d3
wasifz9 <wasif.zulkernine@mail.utoronto.ca>
2021-06-09 09:25:58 -0400

    vue folder

--- model/flask-vue.py
+++ model/flask-vue.py
@@ -1,2 +1,10 @@
-x = 1+1 
-print(x)
+#imports 
+import math 
+import pygame
+
+#definitions 
+WIDTH = 800
+
+#window 
+window = pygame.display.set_mode((WIDTH,WIDTH)) 
+pygame.display.set_caption('A* Path Finding Algorithm')
